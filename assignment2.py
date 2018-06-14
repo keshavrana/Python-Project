3040b24e776cb7401edb1ad4ec6e0ba6f7d2542f
keshav rana <keshavrana396@gmail.com>
2018-06-15 00:46:14 +0530

    print command

---
+++ assignment2.py
@@ -0,0 +1,41 @@
+# Assignment 2
+#Q.1- Print anything you want on screen.
+print("my name is keshav rana")
+print("""my name is keshav""")
+
+#Q.2- Join two strings using '+'.
+#E.g.-"Acad"+"View”
+a="acad"
+b="view"
+c=a+b
+print(c)
+print(type())
+
+# Q.3- Take the input of 3 variables x, y and z . Print their values on screen.
+x=int(input("enter a number: "))
+y=int(input("enter another number: "))
+z=int(input("enter another number: "))
+print(x)
+print(y)
+print(z)
+
+#Q.4- Print “Let’s get started” on screen.
+print('let\'s get started') 
+
+#Q.5- Print the following values using placeholders.
+#s=”Acadview”
+#course=”Python”
+#fees=5000
+s=”Acadview”
+course=”Python”
+fees=5000
+print("%s\n%s\n%d\n"%(s,course,fees))
+
+
+#Q.6- Let’s do some interesting exercise:
+#name=”Tony Stark”
+#salary=1000000
+#print(‘%s’’%d’%(name,salary)
+name="tony stark"
+salary=1000000
+print("%s\n%d\n"%(name,salary)) 

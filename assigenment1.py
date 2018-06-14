53baecf3fb5ffeae6d5a06b5c97273b69aecdcff
keshav rana <keshavrana396@gmail.com>
2018-06-14 20:33:46 +0530

    git commands

---
+++ assigenment1.py
@@ -0,0 +1,12 @@
+# git commands
+git init = create a new local repository.
+git status = to check recent activity.
+git add = to add untracked files. 
+git commit -m"message" = to print message on git project.
+git config --global user.name"your name" = to add your name in git.
+git config --global user.email"your email" = to add your email in git.
+git log = information who use that git
+git remote add origin <server> = to connect with repository.
+git remote show origin = check the activity.
+git push origin :<branch name> = to push code online.
+git push origin master = to push code online.
